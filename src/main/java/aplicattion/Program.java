f14ae534e7027379694c8a2ab9b97a6c502abe64
Júlio César <juliocfds20@gmail.com>
2023-04-28 20:59:25 -0300

    primeiro acesso

---
+++ src/main/java/aplicattion/Program.java
@@ -0,0 +1,27 @@
+package aplicattion;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		Pessoa p = em.find(Pessoa.class, 12);
+
+		System.out.println(p);
+
+		System.out.println("Pronto!");
+		
+		emf.close();
+		em.close();
+
+	}
+
+}
